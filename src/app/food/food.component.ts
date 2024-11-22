d51900d4175d64a9ed4c45771eb7e726cd5aefe0
Nathi33 <nathi33.drnt@outlook.com>
2024-11-22 18:51:45 +0100

    add food-page

--- src/app/food/food.component.ts
+++ src/app/food/food.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataCraftsmanService } from '../data-craftsman.service';
+import { Artisans } from '../models/artisans.model';
 
 @Component({
   selector: 'app-food',
   templateUrl: './food.component.html',
-  styleUrl: './food.component.scss'
+  styleUrl: './food.component.scss',
 })
-export class FoodComponent {
+export class FoodComponent implements OnInit {
+  data: Artisans[] = [];
 
+  constructor(private dataCraftsmanService: DataCraftsmanService) {}
+
+  ngOnInit(): void {
+    this.dataCraftsmanService
+      .getByCategory('Alimentation')
+      .subscribe((response) => {
+        this.data = response;
+      });
+  }
+  selectedArtisans: any = null;
+
+  selectArtisans(artisans: any): void {
+    this.selectArtisans = artisans;
+  }
 }
