17898c2f93dfd1e806ef3b7ea5a07058a5a1672d
Nathi33 <nathi33.drnt@outlook.com>
2024-11-22 18:33:45 +0100

    add manufacturing-page

--- src/app/manufacturing/manufacturing.component.ts
+++ src/app/manufacturing/manufacturing.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataCraftsmanService } from '../data-craftsman.service';
+import { Artisans } from '../models/artisans.model';
 
 @Component({
   selector: 'app-manufacturing',
   templateUrl: './manufacturing.component.html',
-  styleUrl: './manufacturing.component.scss'
+  styleUrl: './manufacturing.component.scss',
 })
-export class ManufacturingComponent {
+export class ManufacturingComponent implements OnInit {
+  data: Artisans[] = [];
 
+  constructor(private dataCraftsmanService: DataCraftsmanService) {}
+
+  ngOnInit(): void {
+    this.dataCraftsmanService
+      .getByCategory('Fabrication')
+      .subscribe((response) => {
+        this.data = response;
+      });
+  }
+  selectedArtisans: any = null;
+
+  selectArtisans(artisans: any): void {
+    this.selectArtisans = artisans;
+  }
 }
